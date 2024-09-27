b9592b203a271ecf8b2c33f648cb6af6c240a9f4
Ted Barbier <106655971+TedBarbier@users.noreply.github.com>
2024-09-27 17:24:10 +0200

    add pygame in main

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from models.map import Map
+import pygame
+from pygame.locals import *
 
+
+pygame.init()
+
+fenetre = pygame.display.set_mode((640, 480))
+
+
+
+continuer = True
+while continuer :
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            continuer = False
+    pygame.display.update()
+pygame.quit()
 def main():
     pass
