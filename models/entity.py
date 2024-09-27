00479b701f02d90d3381072ed10d70859613bdcf
Ted Barbier <106655971+TedBarbier@users.noreply.github.com>
2024-09-27 17:23:47 +0200

    Actual final class Entity with method isAlive

--- models/entity.py
+++ models/entity.py
@@ -1,3 +1,14 @@
-class Entity:
-    def __init__(self,position):
-        self.position=position
+class Entity():
+    def __init__(self, position, name, hp, size, representation):
+        self.position = position
+        self.name = name
+        self.hp = hp
+        self.size = size
+        self.representation = representation
+
+
+    def is_alive(self):
+        return self.hp > 0
+    
+    ##def attacking(self, entity):
+    ##    entity.hp -= self.attack
