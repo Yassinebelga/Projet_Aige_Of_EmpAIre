3574c467a5eb901344062ecb27d6a0aa980c41e3
Ted Barbier <106655971+TedBarbier@users.noreply.github.com>
2024-10-11 15:25:22 +0200

    move difficulty and Create Team.py, modify Entity to add attribute Team

--- models/entity.py
+++ models/entity.py
@@ -1,5 +1,5 @@
 class Entity():
-    def __init__(self, position, name, SQ_size, representation):
+    def __init__(self, position, name, SQ_size, representation, team):
         self.position = position
         self.name = name
         self.SQ_size = SQ_size
