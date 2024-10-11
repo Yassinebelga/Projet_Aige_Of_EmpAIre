e965c2dc76e7153ed6efaa6342230db0f2fd6c70
Ted Barbier <106655971+TedBarbier@users.noreply.github.com>
2024-10-11 15:15:15 +0200

    remove hp from entity so remove isalive()

--- models/entity.py
+++ models/entity.py
@@ -1,14 +1,6 @@
 class Entity():
-    def __init__(self, position, name, hp, SQ_size, representation):
+    def __init__(self, position, name, SQ_size, representation):
         self.position = position
         self.name = name
-        self.hp = hp
         self.SQ_size = SQ_size
         self.representation = representation
-
-
-    def is_alive(self):
-        return self.hp > 0
-    
-    ##def attacking(self, entity):
-    ##    entity.hp -= self.attack
