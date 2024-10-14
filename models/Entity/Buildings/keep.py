bbc66c1d276310edb8ae6bdd49d554ebc1675d79
Ted Barbier <106655971+TedBarbier@users.noreply.github.com>
2024-10-14 09:53:10 +0200

    correction class Building and inherit class of building
    and reorganisation order args Unit and inherit class of unit

--- models/Entity/Buildings/keep.py
+++ models/Entity/Buildings/keep.py
@@ -1,5 +1,5 @@
 from building import Building
 
 class Keep(Building):
-    def __init__(player ,build, SQ_size, build_time, cost, hp, walkable, drop_point, population):
-        super().__init__(player, SQ_size=1, build_time=80, cost={"W":35,"G":125}, build=False, hp=800, walkable=False, drop_point=False)
+    def __init__(self, position, team, name = "Keep", representation = "K", SQ_size = 1, hp = 800, cost = {"W" : 35, "G" : 125}, build_time = 80, walkable = False):
+        super().__init__(position, team, name, representation, SQ_size, hp, cost, build_time, walkable)
--- models/Entity/Buildings/towncenter.py
+++ models/Entity/Buildings/towncenter.py
@@ -1,8 +1,11 @@
 from building import Building
 
 class TownCenter(Building):
-    def __init__(player ,build, SQ_size, build_time, cost, hp, walkable, drop_point, population):
-        super().__init__(player, SQ_size=4, build_time=150, cost={"W":350}, build=False, hp=1000, walkable=False, drop_point=True, population=5)
+    def __init__(self, position, team, name = "Town Center", representation = "T", SQ_size = 4, hp = 1000, cost = {"W" : 350}, build_time = 150, walkable = False, unit_spawn = None, drop_point = True, population = 5):
+        super().__init__(position, team, name, representation, SQ_size, hp, cost, build_time, walkable)
+        self.unit_spawn = unit_spawn
+        self.drop_point = drop_point
+        self.population = population
 
     def spawn():
         pass
