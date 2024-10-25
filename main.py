547fe946baaea7ec28e8716a8da5403fc66af93a
amauol <chau.amaury@gmail.com>
2024-10-25 16:21:06 +0200

    initalisation d'un delta_time pour la gestion d'évènements spécifique

--- main.py
+++ main.py
@@ -14,15 +14,19 @@ clock = pygame.time.Clock()
 # création d'un tableau pour stocké le temps de chaque évènement
 event_times = []
 FPS = 60 # initlisation du nombre de FPS/de tick de Clock par seconde
+previous_time = pygame.time.get_ticks()
 
 continuer = True
 while continuer :
+    current_time = pygame.time.get_ticks()
+    delta_time = (current_time-previous_time) / 10000.0 # initalisation d'un temps delta qui va gérer des évènements spécifiques comme la vitesse de déplacement des unités ou la vitesse de construction des bâtiments 
+    previous_time = current_time
     for event in pygame.event.get():
         event_time = pygame.time.get_ticks()
         event_times.append(event_time) 
         if event.type == QUIT:
             continuer = False
-    current_time = pygame.time.get_ticks()
+
 
     # Mise à jour du jeux 
 
