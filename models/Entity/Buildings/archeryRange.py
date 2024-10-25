8c9ee3635eda368f968b6c6a17ea495461dcaea4
Paul Davesne <paul.davesne@insa-cvl.fr>
2024-10-25 15:57:14 +0200

    merge avec main

--- models/Entity/Buildings/archeryRange.py
+++ models/Entity/Buildings/archeryRange.py
@@ -1,19 +1,9 @@
 from building import Building
 
 class archeryRange(Building):
-<<<<<<< HEAD
-    def __init__(self, player ,build, SQ_size, representation, build_time, cost, hp, walkable, drop_point, population):
-        super().__init__(player, SQ_size=3, representation="A", build_time=50, cost={"W":175}, build=False, hp=500, walkable=False, drop_point=False)
-
-    def spawn_archers(self):
-        if self.build:
-            pass
-        
-=======
     def __init__(self, position, team, name = "Archery Range", representation = "A", SQ_size = 3, hp = 500, cost = {"W" : 175}, build_time = 50, walkable = False, unit_spawn = None):
         super().__init__(position, team, name, representation, SQ_size, hp, cost, build_time, walkable)
         self.unit_spawn = unit_spawn
         
     def spawn_archers():
         pass
->>>>>>> 372ab19cbfd441ee86fdb1b044712fa73c5c349b
--- models/Entity/Buildings/farm.py
+++ models/Entity/Buildings/farm.py
@@ -1,12 +1,7 @@
 from building import Building
 
 class Farm(Building):
-<<<<<<< HEAD
-    def __init__(self, player ,build, SQ_size, representation, build_time, cost, hp, walkable, drop_point, population):
-        super().__init__(self, player, SQ_size=2, representation="F", build_time=10, cost={"W":60}, build=False, hp=100, walkable=True, drop_point=False, food={"F":300})
-=======
     def __init__(self, position, team, name = "Farm", representation = "F", SQ_size = 2, hp = 100, cost = {"W" : 60}, build_time = 10, walkable = True, inventory = {"F" : 0}, max_food = 300):
         super().__init__(position, team, name, representation, SQ_size, hp, cost, build_time, walkable)
         self.inventory = inventory
         self.max_food = max_food
->>>>>>> 372ab19cbfd441ee86fdb1b044712fa73c5c349b
