f6674a3ad016601b54c45ea1ffde3c274bcd1e15
Paul Davesne <paul.davesne@insa-cvl.fr>
2024-12-17 15:13:13 +0100

    updated the tree node, made the ctrl and deco nodes and made the sequence, invert and forcefail nodes (WIP)

--- garbage/Paul/garbage.py
+++ garbage/Paul/garbage.py
@@ -1,27 +1,42 @@
 class treeNode:
 
-    def _init_(self, status):
-        pass
-
-    def start():
-        pass
+    def __init__(self, status):
+        self.status=status
     
-    def update():
-        pass
-
+    def update(self):
+        return self.status
+    
+    def start(self):
+        self.update(self)
 
 class ctrlNode(treeNode):
-    pass
+
+    def __init__(self, status, childlist):
+        self.status=status
+        self.childlist=childlist                #a ctrl node has several children, thus implemented as a list
 
 class decoNode(treeNode):
-    pass
+
+    def __init__(self, status, child):
+        self.status=status
+        self.child=child                        #a deco node has only one child
+
 
 class leafNode(treeNode):
-    pass
+    pass                                        #a leaf node has no children
 
 class Sequence(ctrlNode):
-    pass
 
+    def update(self):
+        for (child) in self.childlist:
+            if (child.update(child)!="success"): #if the child process returns anything but success, the sequence stops updating its branches and returns failure
+                self.status="failure"
+                return self.status
+        
+        self.status="success"  #else, the sequence updates all of its children and returns success 
+        return self.status
+
+    
 class Fallback(ctrlNode):
     pass
 
@@ -32,10 +47,20 @@ class UntilFail(decoNode):
     pass
 
 class ForceFail(decoNode):
-    pass
+    
+    def update(self):
+        self.child.update(self.child) #I know theres a mistake here, working on it
+        self.status="failure"
+        return self.status
 
 class Invert(decoNode):
-    pass
+    
+    def update(self):
+        if (self.child.update(self.child)=="success"):
+            self.status="failure"
+        elif (self.child.update(self.child)=="failure"):
+            self.status="success"
+        return self.status       
 
 class Action(leafNode):
     pass
